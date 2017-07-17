cb61b83b19f209e1f02ef1bada4b6421ca7f793a
guiloor <guille-497@hotmail.com>
2017-07-17 18:41:30 +0200

    Add files via upload

---
+++ estructura/estructura.cpp
@@ -0,0 +1,23 @@
+#include <studio.h>
+
+#define ALUMNOS 10
+
+struct alumno{
+
+	char nombre[20];
+	char apellido[20];
+	int clase;
+
+};
+
+struct alumno alumno[ALUMNOS];
+
+main(){
+	int num_alumno;
+	for( num_alumno=0; num_alumno<=ALUMNOS; num_alumno++){
+	printf("\n el numero de amigos es %i: \n. num_amigo");
+
+	}
+
+	return 0;
+}
