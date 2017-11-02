51feb7dfa127704ae4157e304cccb8209cbbbfa5
guiloor <guille-497@hotmail.com>
2017-11-02 13:10:40 +0100

    2017

---
+++ 2017/00_primitivas/hi.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+system("clear");
+system("toilet -fpagga --gay The world is a vampire");
+
+
+printf("The world is a vampire \n");
+
+    return EXIT_SUCCESS;
+}
---
+++ 2017/00_primitivas/nombre.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int
+main()
+{
+
+ std::cout << "The World is a Vampire!" << std::endl;
+
+ return 0;
+
+}
