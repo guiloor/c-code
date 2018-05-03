3e3a54560f4be12fa7c8d4b063fb11ccb3a155c6
guiloor <guille-497@hotmail.com>
2018-05-03 14:37:57 +0200

    correccion

--- 2017/Games/Snake/main.cpp
+++ 2017/Games/Snake/main.cpp
@@ -1,7 +1,6 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 
 #define V 21
 #define H 65
@@ -21,59 +20,25 @@ snk snake[N]; // el cuerpo de la serpiente
 frt fruta;
 
 void inicio(int *tam, char campo[V][H]);
-void intro_Campo(char campo[V][H]);
 void intro_Datos(char campo[V][H], int tam);
-void dibujar (char campo[V][H]);
 
-	
-int main(){
 
-	int tam;
-	char campo[V][H]; //matriz donde recogeremos todos los elementos
-	
-	inicio(&tam, campo); //Iniciar todos nuestros elementos
-	dibujar(campo);
-	 system("pause");
-	
-    return 0;
-}
+void dibujar (char campo[V][H]){
+	int i,j;
 
-void inicio(int *tam, char campo[V][H]){
-	int i;
-	//La cabeza de nuestra serpiente
-	snake[0].x = 32;
-	snake[0].y = 10;
-	
-	//Tamaño de la serpiente
-	*tam = 4;
-	
-	//Corrdenadas Fruta
-	 srand(time(NULL));
-	
-	fruta.x = rand() % (H - 2) + 1;  
-	fruta.y = rand() % (V - 2) + 1; 
-	
-	// El borde es 0 y la muerte, por eso si la fruta aparece en el borde moririamos.
-	while(fruta.x == 0){
-			fruta.x = rand() % (H -1);
-	}	
-	while(fruta.y == 0){
-			fruta.y = rand() % (V -1);
+	for(i = 0; i < V; i++){
+		for (j = 0; j < H; j++){
+			printf("%c", campo[i][j]);
+		}
+		printf("\n");
 	}
-	
-	for (int i = 0; i < *tam; i++){
-		snake[i].ModX = 1;
-		snake[i].ModY = 0;		
-	}	
-		
-		intro_Campo(campo);
-		intro_Datos(campo,*tam);
+
 }
 
 //Creacion del campo de juego
 void intro_Campo(char campo[V][H]){
 	 int i, j;
-	 
+
 	  for (i = 0 ; i < V; i++){
 	  		 for (j = 0; j < H; j++){
 	  		 	 if (i == 0 || i == V - 1){
@@ -81,7 +46,7 @@ void intro_Campo(char campo[V][H]){
 		}
 		else if (j == 0 || j == H - 1){
 		 	 campo [i][j] = '|';
-		}	
+		}
 		else if (j == 0 || j == H -1){
 			campo[i][j] = '|';
 		}
@@ -92,36 +57,76 @@ void intro_Campo(char campo[V][H]){
 	}
 }
 
+void inicio(int *tam, char campo[V][H]){
+	//La cabeza de nuestra serpiente
+	snake[0].x = 32;
+	snake[0].y = 10;
+
+	//Tamaño de la serpiente
+	*tam = 4;
+
+	//Corrdenadas Fruta
+	 srand(time(NULL));
+
+	fruta.x = rand() % (H - 2) + 1;
+	fruta.y = rand() % (V - 2) + 1;
+
+	// El borde es 0 y la muerte, por eso si la fruta aparece en el borde moririamos.
+	while(fruta.x == 0){
+			fruta.x = rand() % (H -1);
+	}
+	while(fruta.y == 0){
+			fruta.y = rand() % (V -1);
+	}
+
+	for (int i = 0; i < *tam; i++){
+		snake[i].ModX = 1;
+		snake[i].ModY = 0;
+	}
+
+		intro_Campo(campo);
+		intro_Datos(campo,*tam);
+}
+
 //Meter todos los datos en la matriz campo
-void intro_datos(char campo[V][H], int tam){
+void intro_Datos(char campo[V][H], int tam){
 	int i;
-	
+
 	for (i = 0; i < tam; i++){
 		snake[i].x= snake[i -1].x - 1;
 		snake[i].y = snake[i -1].y;
-		
+
 		snake[i].imagen = 'X';
 	}
-	
+
 	snake[0].imagen = 'O';
-	
+
 	for(i = 0; i < tam; i++){
 		campo[snake[i].y][snake[i].x] = snake[i].imagen;  //meter dentro de la matriz las cordenadas de de la serpiente
 	}
 	campo[fruta.y][fruta.x] = '%';
 }
 
-void dibujar (char campo[V][H]){
-	int i,j;
-	
-	for(i = 0; i < V; i++){
-		for (j = 0; j < H; j++){
-			printf("%c", campo[i][j]);
-		}
-		printf("\n");
-	}
-	
+
+int main(){
+
+	int tam;
+	char campo[V][H]; //matriz donde recogeremos todos los elementos
+
+	inicio(&tam, campo); //Iniciar todos nuestros elementos
+	dibujar(campo);
+	 system("pause");
+
+    return EXIT_SUCCESS;
 }
+
+
+
+
+
+
+
+
 
 
 
